6237572f5c9a82c61848da7d606422bf7ee9c145
Shreesh Bhat <shreeshbhat@users.noreply.github.com>
2021-07-14 18:33:05 -0400

    Load emoji in Nav bar dynamically
    - Load NavEmoji component during client time
      instead of inserting emoji at build time

---
+++ src/components/NavEmoji.svelte
@@ -0,0 +1,16 @@
+<script>
+import { onMount } from 'svelte';
+import { getRandomEmoji } from '../utils/emoji';
+
+let emoji = '';
+onMount(() => {
+    emoji = getRandomEmoji();
+});
+</script>
+<style lang="scss">
+    .emoji {
+        font-size: var(--h1-text-size);
+    }
+</style>
+
+<span aria-hidden="true" class="emoji">{emoji}</span>
